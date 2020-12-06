d976dfc0728c2d400947d56a2a1b944885749547
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-12-06 14:30:28 +0300

    HotelFinder Entity ve DataAccess katmanları yazılıyor.

---
+++ 02_HotelFinder/HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        /*
+         * Microsoft.EntityFrameworkCore.SqlServer installed
+         * Microsoft.EntityFrameworkCore.Tools     installed : migration kullanabilmek için projeye dahil ettik.
+         */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=IMPORTANTPC\KAMILKAPLAN; Database=HotelDb;uid=sa;pwd=1");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
