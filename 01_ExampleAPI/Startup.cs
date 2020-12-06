e8290314b435ad5d4c00a7954d6b352ab93467de
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-12-06 03:18:04 +0300

    webAPI projesi oluşturuldu.

---
+++ 01_ExampleAPI/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _01_ExampleAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("<div style='text-align: center; color: chocolate; border: 1px solid red; padding: 1 %; '>KAMIL KAPLAN</div>");
+                });
+            });
+        }
+    }
+}
